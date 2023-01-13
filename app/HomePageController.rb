dff54e532ab444dfc35d6ae2ae2f2aa0bef5338b
Graypegg <me@graypegg.com>
2023-01-12 23:50:14 -0500

    better controllers, made a little guess the number game for a POC

--- app/HomePageController.rb
+++ app/HomePageController.rb
@@ -2,10 +2,34 @@
 
 class HomePageController < Controller
   def get
-    respond_with "Did this work?"
+    respond_with "
+# Here's a dumb guess a number game
+
+=> /game/play
+    "
   end
 
-  def answer(question)
-    redirect_to '/'
+  def play_get
+    ask_for "Think of a number between 1 and 10 (inclusive)"
+  end
+
+  def play_answer(number)
+    guess = number.to_i
+    if guess > 0 && guess <= 10
+      answer = rand(9) + 1
+      respond_with "
+# The answer was #{answer}
+
+#{answer == guess ? 'You win!' : 'You lose'}
+
+=> /game/play Try again
+    "
+    else
+      respond_with "
+# That's not a valid number :(
+
+=> /game/play Try again
+    "
+    end
   end
 end
--- framework/app.rb
+++ framework/app.rb
@@ -9,16 +9,30 @@ class App
   end
 
   def respond
-    controller = CONTROLLERS[@connection.request.path]
+    segments = split_path_by_segments @connection.request.path
+    route = segments[1]
+    controller = CONTROLLERS[route]
+
     if controller
       instance = Kernel.const_get(controller).new self
 
-      if @connection.input? && instance.respond_to?(:answer)
-        @connection.log_append "#{controller}#answer"
-        response = instance.answer @connection.input
-      elsif !@connection.input? && instance.respond_to?(:get)
-        @connection.log_append "#{controller}#get"
-        response = instance.get
+      action = segments[2]
+
+      action = nil if action == ''
+
+      answer_symbol = "#{action}_answer" if action
+      answer_symbol = 'answer' unless action
+      get_symbol = "#{action}_get" if action
+      get_symbol = 'get' unless action
+
+      if @connection.input? && instance.respond_to?(answer_symbol.to_sym)
+        @connection.log_append "#{controller}##{answer_symbol}"
+        response = instance.send answer_symbol.to_sym, @connection.input
+      elsif !@connection.input? && instance.respond_to?(get_symbol.to_sym)
+        @connection.log_append "#{controller}##{get_symbol}"
+        response = instance.send get_symbol.to_sym
+      else
+        @connection.close_with_error
       end
 
       if response
@@ -28,7 +42,13 @@ class App
 
       @connection.send unless @connection.open?
     else
-      connection.close_with_error
+      @connection.close_with_error
     end
+  end
+
+  private
+
+  def split_path_by_segments(path)
+    path.match %r{/([\w-]*)/?([\w-]*)?}
   end
 end
